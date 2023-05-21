b43809a9172b1800da046671a08020281565c248
Gaspar Sio <sio.gaspar@gmail.com>
2023-05-22 00:40:47 +0200

    ADD: script.js - New changes on .scss

--- script.js
+++ script.js
@@ -9,12 +9,28 @@ const formBtn = document.querySelector('.send-button');
 const clickLogo = logo.addEventListener('click', ()=>{
     location.reload();
 })
+
 //RELOAD WEB CLICK FORM BUTTON
 const clickFormBtn = formBtn.addEventListener('click', ()=>{
     location.reload();
 })
 
+//PERCENTAGE SCROLLER
+document.addEventListener('DOMContentLoaded', () => {
+    const progressBarPer = document.querySelector('.progress-bar__inner')
+    window.addEventListener('scroll', () => {
+        let h = document.documentElement;
+        let scrollTop = h.scrollTop || document.body.scrollTop;
+        let scrollHeight = h.scrollHeight || document.body.scrollHeight;
+        let percentage = scrollTop / (scrollHeight - h.clientHeight) * 100;
+        let percentRound = Math.round(percentage)
+        // progressBarPer.innerHTML = percentRound + '%';
+        progressBarPer.style.width = percentage + '%';
+        console.log(percentage)
+    })
+})
 
+//CLICK HAMBURG MENU
 const clickHamb = mobImg.addEventListener('click', () => {
     modal.style.display = "block";
     crossImg.style.display = "block";
