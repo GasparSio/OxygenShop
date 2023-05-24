f3f9e26e5a6b1d23757fb5da8fd484214790a2f8
Gaspar Sio <sio.gaspar@gmail.com>
2023-05-24 16:23:52 +0200

    New selector currency using fetch

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-//FORM VALIDATION
+//FORM VALIDATION & EMAIL SUBMIT
 const nameForm = document.getElementById('name')
 const emailForm = document.getElementById('email')
 const form = document.getElementById('form')
@@ -18,7 +18,6 @@ const validateForm = (event) => {
             if(expresiones.nameUsuario.test(event.target.value)){
                 nameForm.style.borderColor = '#55DFB4';
                 campos.nameUsuario = true;
-                console.log(event.target.value)
             }else{
                 nameForm.style.borderColor = '#EB476E';
                 campos.nameUsuario = false;
@@ -162,8 +161,6 @@ document.addEventListener('DOMContentLoaded', () => {
     setTimeout(() => {
         showModal();
     }, 5000);
-    console.log('hola')
-    console.log(localStorage.getItem('modalClosed'))
 })
 // Mostrar el modal
 function showModal() {
@@ -172,9 +169,7 @@ function showModal() {
         modal.style.display = 'none';
     }else{
         modal.style.display = 'block';
-        console.log('que mierda pasa')
     }
-    console.log(localStorageValue);
 }
 
 // Cerrar el modal
@@ -191,6 +186,7 @@ closeButton.addEventListener('click', () => {
 //Cerrar el modal haciendo clic fuera del modal
 window.addEventListener('click', (event) => {
   if (event.target == modal) {
+    console.log(event.target);
     closeModal();
   }
 });
@@ -213,3 +209,154 @@ if (scrollPosition > (pageHeight * 0.25)) {
 });
 
 
+//CURRENCY SELECTOR
+const btnUsd = document.querySelector('.section-currency__dolar');
+const btnEuro = document.querySelector('.section-currency__euro');
+const btnPound = document.querySelector('.section-currency__pound');
+
+const pricesToChange = [
+    document.getElementById("price-number__card1"),
+    document.getElementById("price-number__card2"),
+    document.getElementById("price-number__card3"),
+];
+    
+const priceNumCard1 = document.getElementById("price-number__card1");
+const priceNumCard2 = document.getElementById("price-number__card2");
+const priceNumCard3 = document.getElementById("price-number__card3");
+
+
+
+// Función para obtener el tipo de cambio de la API y actualizar los valores
+  const getExchangeRate = async (currency) => {
+    const baseURL =
+      "https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/";
+    const urlToFetch = baseURL + currency;
+  
+    try {
+      const response = await fetch(urlToFetch);
+      if (response.ok) {
+        const jsonResponse = await response.json();
+        console.log(jsonResponse);
+        return jsonResponse;
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  
+async function exchangeCurrency (currency) {
+    // Getting which is the currently displayed currency in the HTML
+  let currentCurrency = '';
+  if (priceNumCard1.innerText.includes("$")) {
+    currentCurrency = "usd";
+  } else if (priceNumCard1.innerText.includes("€")) {
+    currentCurrency = "eur";
+  } else {
+    currentCurrency = "gbp";
+  }
+
+
+  // Getting which is the new currency based on the user click
+  let newCurrency = currency;
+
+  // If the user clicks on USD
+  if (newCurrency == "usd.json") {
+    // Change from EUR to USD
+    if (currentCurrency == "eur") {
+      // Getting the exchange rate from EUR to USD, getting the current displayed value in the HTML and multiplying it by the exchange rate
+      const eurToUsd = await getExchangeRate("eur/usd.json");
+      pricesToChange.forEach((price) => {
+        price.innerText =
+          "$ " +
+          (
+            parseFloat(price.innerText.replace(/[^0-9\.-]+/g, "")) *
+            eurToUsd.usd
+          ).toFixed(2);
+      });
+      // Change from GBP to USD
+    } else if (currentCurrency == "gbp") {
+      const gbpToUsd = await getExchangeRate("gbp/usd.json");
+      pricesToChange.forEach((price) => {
+        price.innerText =
+          "$ " +
+          (
+            parseFloat(price.innerText.replace(/[^0-9\.-]+/g, "")) *
+            gbpToUsd.usd
+          ).toFixed(2);
+      });
+    }
+
+    // If the user clicks on EUR
+  } else if (newCurrency == "eur.json") {
+    // Change from USD to EUR
+    if (currentCurrency == "usd") {
+      const usdToEur = await getExchangeRate("usd/eur.json");
+      pricesToChange.forEach((price) => {
+        price.innerText =
+          "€ " +
+          (
+            parseFloat(price.innerText.replace(/[^0-9\.-]+/g, "")) *
+            usdToEur.eur
+          ).toFixed(2);
+      });
+      // Change from GBP to EUR
+    } else if (currentCurrency == "gbp") {
+      const gbpToEur = await getExchangeRate("gbp/eur.json");
+      pricesToChange.forEach((price) => {
+        price.innerText =
+          "€ " +
+          (
+            parseFloat(price.innerText.replace(/[^0-9\.-]+/g, "")) *
+            gbpToEur.eur
+          ).toFixed(2);
+      });
+    }
+
+    // If the user clicks on GBP
+  } else {
+    // Change from USD to GBP
+    if (currentCurrency == "usd") {
+      const usdToGbp = await getExchangeRate("usd/gbp.json");
+      pricesToChange.forEach((price) => {
+        price.innerText =
+          "£ " +
+          (
+            parseFloat(price.innerText.replace(/[^0-9\.-]+/g, "")) *
+            usdToGbp.gbp
+          ).toFixed(2);
+      });
+      // Change from EUR to GBP
+    } else if (currentCurrency == "eur") {
+      const eurToGbp = await getExchangeRate("eur/gbp.json");
+      pricesToChange.forEach((price) => {
+        price.innerText =
+          "£ " +
+          (
+            parseFloat(price.innerText.replace(/[^0-9\.-]+/g, "")) *
+            eurToGbp.gbp
+          ).toFixed(2);
+      });
+    }
+  }
+}
+//CLICK TO THE BUTTONS
+btnEuro.addEventListener('click', () => {
+    btnEuro.classList.add('active');
+    btnUsd.classList.remove('active');
+    btnPound.classList.remove('active');
+    exchangeCurrency("eur.json");
+    
+})
+btnUsd.addEventListener('click', () => {
+    btnUsd.classList.add('active');
+    btnEuro.classList.remove('active');
+    btnPound.classList.remove('active');
+    exchangeCurrency("usd.json");
+})
+btnPound.addEventListener('click', () => {
+    btnPound.classList.add('active');
+    btnUsd.classList.remove('active');
+    btnEuro.classList.remove('active');
+    exchangeCurrency("gbp.json");
+})
